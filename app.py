ec495fd78a21173be864e99b48131829eef6e394
BrunaDeAraujo <baby.blue3940@gmail.com>
2024-10-23 20:22:32 -0300

    from-import

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from modelos.restaurante import Restaurante
 restaurante_praca = Restaurante('praça', 'Gourmet')
 restaurante_praca.receber_avalicao('gui', 10)
 restaurante_praca.receber_avalicao('Bru', 8)
-restaurante_praca.receber_avalicao('tutu', 6)
+restaurante_praca.receber_avalicao('tutu', 9)
 
 
 def main():
