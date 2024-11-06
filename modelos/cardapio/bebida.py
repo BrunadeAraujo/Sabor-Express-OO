53a02360e32d4021fa2a6a95f5948b2e0f15cf1e
BrunaDeAraujo <baby.blue3940@gmail.com>
2024-11-05 23:52:39 -0300

    polimorfismo- método abstrato

--- modelos/cardapio/bebida.py
+++ modelos/cardapio/bebida.py
@@ -5,5 +5,12 @@ class Bebida(ItemCardapio):
        super().__init__(nome,preco)
        self.tamanho = tamanho
 
-def __str__(self):
-    return self._nome
+    def __str__(self):
+     return self._nome
+    
+    def aplicar_desconto(self):
+       self._preco -= (self._preco * 0.08)
+
+       
+
+   
